da5b157ff85c7a3bbb27acd5e1af892372fb49df
Diego Moncada <tank_mac@Tests-MacBook-Pro.local>
2018-06-06 21:59:40 -0400

    filter added and some refactoring from webapp/views.py to webapp/models.py

--- mysite/webapp/models.py
+++ mysite/webapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from datetime import datetime, date
 import json
+import pytz
 
 # Create your models here.
 
@@ -11,23 +13,43 @@ class SensorType(models.Model):
 	def __str__(self):
 		return self.name
 
+class SensorManager(models.Manager):
+	def get_all_json(self, filter_date):
+		sensors_all = self.all()
+		
+
+		json_all = []
+		for item in sensors_all:
+			json_all.append(item.get_json_with_relations(filter_date))
+		return json_all
+
 class Sensor(models.Model):
+	objects = SensorManager()
 	sensorType = models.ForeignKey(SensorType, on_delete=models.CASCADE)
 	given_name = models.CharField(max_length=50)
 	id_number = models.CharField(max_length=50)
 
-	def get_json_with_relations(self):
+	def get_json_with_relations(self, filter_date):
+
 		return {
 			"sensor_type_name":self.sensorType.name,
 			"sensor_type_unit":self.sensorType.unit,
 			"sensor_type_measure":self.sensorType.measure,
 			"given_name":self.given_name,
 			"id_number":self.id_number,
-			'entries':self.get_entries_from_sensor_json()
+			'entries':self.get_entries_from_sensor_json(filter_date)
 		}
 
-	def get_entries_from_sensor_json(self):
+	def get_entries_from_sensor_json(self,filter_date):
 		entries_objects = Entry.objects.filter(sensor=self.id)
+
+		today_date = datetime.now()
+		timezone = pytz.timezone("GMT")
+		today_date = timezone.localize(today_date)
+		
+		if (not filter_date==None and filter_date < today_date):
+			entries_objects = entries_objects.filter(created_at__range=(filter_date,today_date))
+
 		entries_json = []
 		for i in entries_objects:
 			entries_json.append(i.get_json())
--- mysite/webapp/views.py
+++ mysite/webapp/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from django.core import serializers
-
-from .models import Entry, SensorType, Sensor
-
+from datetime import datetime
+from django.utils.dateparse import parse_datetime
+from .models import Entry, SensorType, Sensor, SensorManager
 
 # Create your views here.
 def index(request):
@@ -12,10 +12,29 @@ def index(request):
 	# 	{'id':1,'type_name':'ds18b20','unit':'degrees','measure':'temperature','given_name':'ferm temp 1', 'id_number':'28-123123',
 	# 	'entries':[{'value':20,'created_at':'test date'},{'value':21,'created_at':'test date'},{'value':22,'created_at':'test date'}]}
 	# ]
-	sensors = Sensor.objects.all()
+	# sensors = Sensor.objects.all()
 	# data = serializers.serialize('json', entry_list)
-	data = []
-	for i in sensors:
-		data.append(i.get_json_with_relations())
-	return HttpResponse(json.dumps(data),content_type='application/json')
 	
+	# data = []
+	# for i in sensors:
+		# data.append(i.get_json_with_relations())
+	filter_date = str_to_datetime_default(request.GET.get('date'))
+
+	sensors_json = Sensor.objects.get_all_json(filter_date)
+	return HttpResponse(json.dumps(sensors_json),content_type='application/json')
+	
+def filter(request):
+	date_query = request.GET.get('date')
+	from_datetime = str_to_datetime_default(date_query);
+
+	return HttpResponse('<p>'+date.__str__()+'</p>')
+
+# PRIVATE FUNCTIONS
+
+def str_to_datetime_default(query):
+	if not query:
+		return query
+	query = list(query)
+	query[-6] = '+'
+	query = "".join(query);
+	return parse_datetime(query)
