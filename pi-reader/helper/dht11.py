530760eff9a8a6a9af24d37f1feda2ac03d000c1
Diego <dfmoncada@gmail.com>
2018-06-09 19:08:40 +0000

    added second sensor, with all the logic refactored to allow multiple types of sensors

--- pi-reader/helper/dht11.py
+++ pi-reader/helper/dht11.py
@@ -1,11 +1,17 @@
 import Adafruit_DHT
-
+from datetime import datetime
 
 class DHT11:
     # [options] json with 'pin'= pin_number, type= any of '11','22','2302' ,
+    add_value = ("INSERT INTO webapp_entry "
+                "(sensor_id, value, created_at) "
+                "VALUES (%s, %s, %s)")
+
     def __init__(self, options):
         self.pin = options['pin']
         self.type = options['type']
+        self.temp_id = options['id']['temp']
+        self.humid_id = options['id']['humid']
 
     def read(self):
         sensor_args = {'11': Adafruit_DHT.DHT11,
@@ -13,11 +19,14 @@ class DHT11:
                        '2302': Adafruit_DHT.AM2302}
         sensor = sensor_args[self.type]
         humidity, temperature = Adafruit_DHT.read_retry(sensor, self.pin)
-        return temperature
+        return {'temp':temperature, 'humid':humidity}
 
-    def save(self, ):
+    def save(self, cursor):
         results = self.read()
-        self.results = results
-
-
-
+        now = datetime.now()
+        data_value_temp = (self.temp_id, results['temp'], now) 
+        data_value_humid = (self.humid_id, results['humid'], now)
+        cursor.execute(self.add_value, data_value_temp)
+        cursor.execute(self.add_value, data_value_humid)
+        print("data inserted=> sensor-id:'" + str(self.temp_id) + '-' + self.type + "', value:" + str(results['temp']))
+        print("data inserted=> sensor-id:'" + str(self.humid_id) + '-' + self.type + "', value:" + str(results['humid']))
--- pi-reader/reader.py
+++ pi-reader/reader.py
@@ -3,7 +3,7 @@ import MySQLdb.connections
 import time
 from helper.ds18b20 import DS18B20
 from helper.dht11 import DHT11
-from config import Config_setup
+from config import ConfigSetup
 
 cnx = MySQLdb.connect(user='django', password='django-user-password', database='pidata', host='127.0.0.1', port=3306)
 cursor = cnx.cursor()
@@ -14,9 +14,13 @@ add_value = ("INSERT INTO webapp_entry "
              "(sensor_id, value, created_at) "
              "VALUES (%s, %s, %s)")
 
+sensors = ConfigSetup.get_sensors()
+
 while True:
-    for sensor in Config_setup.sensors:
-        eval(sensor['type']).save(sensor['options'])
+    for sensor in sensors:
+       sensor_class = globals()[sensor['type']]
+       sensor_object = sensor_class(sensor['options'])
+       sensor_object.save(cursor)
 
     # Insert new rows into entries for each sensor
     cnx.commit()
